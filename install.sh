0f9e0d3f79095bd21d54702f86aba3dc8228f110
Bahex <Bahex@users.noreply.github.com>
2016-05-28 04:28:15 +0300

    Fixed a minor issiue with the script.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-[ ! "yes-please" == "$1" ] && exit 1
+[ ! "yes-please" == "$1" ] && echo "If you are sure write \"yes-please\" as the first argument." &&exit 1
 
 INSDIR=$(realpath "$0")
 INSDIR=${INSDIR%/*}
 
 for file in config Xcompose xinitrc xprofile Xresources Xresources.d xserverrc zlogin zshrc; do
-	ln -s ${INSDIR}/${file} ${HOME}/.${file}
+	ln -s ${INSDIR}/${file} ${HOME}/.${file} || echo "$INSDIR olmadı"
 done
 exit 0
